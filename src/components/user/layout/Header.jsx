b5558194b8f84eb2566edb9b60e4ab531776f9be
parthgodhani-dev <dev.parthgodhani@gmail.com>
2025-11-13 20:59:43 +0530

    Fix: headerstyle.css path case issue for Vercel

--- src/components/user/layout/Header.jsx
+++ src/components/user/layout/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./headerstyle.css"
+import "./headerstyle.css";
 import { Container, Navbar, NavDropdown, Nav } from 'react-bootstrap'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
